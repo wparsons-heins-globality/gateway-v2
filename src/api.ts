ee3f9762c0eae8018da97fa7109902f0222243e3
William Parsons-Heins <william.parsons-heins@globality.com>
2024-03-22 10:41:34 +0000

    some notes

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import { RESTDataSource } from '@apollo/datasource-rest'
 import { User } from './generated/graphql'
 
+// NOTE: This would just be the gloclient
 export class ArizaAPI extends RESTDataSource {
     override baseURL = 'https://ariza.dev.globality.io/api/v1/'
 
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { Resolvers } from './generated/graphql'
 import { ArizaAPI } from './api'
 import { User } from './generated/graphql'
 
+// NOTE: Here we would refer to the gloclient
 interface ContextValue {
     dataSources: {
         ariza: ArizaAPI
@@ -16,6 +17,10 @@ const typeDefs = readFileSync('./schema.graphql', { encoding: 'utf-8' })
 
 const resolvers: Resolvers<ContextValue> = {
     Query: {
+        // NOTE: This was manually added to the schema just to show how it would
+        // work, if you rerun the generation it will say this doesn't exist anymore.
+        // We would probably want to define a new graph just for the gateway and use
+        // some sort of schema stitching...
         getUser: async (_, { userId }, { dataSources }): Promise<User> => {
             return await dataSources.ariza.getUser(userId)
         },
